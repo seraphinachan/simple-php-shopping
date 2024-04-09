acb4b42473c18d1c542b6d01f4d59f9976d675b9
root <root@1.234.44.149>
2024-04-09 22:46:19 +0900

    DB 관련 파일 추가

--- dbconfig.php
+++ dbconfig.php
@@ -1,19 +1,41 @@
 <?php
+
+// 2024.04.09 김지연 수정
+
 // $servername = "localhost";
 // $username = "user";
 // $password = "jiyeonyee0312";
 // $dbname = "shopping";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "shopping";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "shopping";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-mysqli_set_charset($conn, "utf8mb4");
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+// mysqli_set_charset($conn, "utf8mb4");
 
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+// if (!$conn) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
+
+require_once __DIR__ . '/vendor/autoload.php'; // Composer autoload
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("데이터베이스 연결 실패: " . $conn->connect_error);
 }
+
+mysqli_set_charset($conn, "utf8mb4");
 
 ?>
